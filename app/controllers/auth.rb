2dcd2e5827d42bd3b2610edcdb7097ed965fd2c7
rosapergams <rosa@pergams.com>
2015-02-18 10:13:19 -0500

    Add auth controller

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,39 @@
 get '/' do
-  erb :welcome
+  erb :'index'
 end
 
 get '/login' do
-  
+  @error = "If at first you don't succeed, try, try again." if params[:error].
+  erb :'auth/login'
+end
+
+post '/login' do
+  user = User.find_by(username: params[:user][:username])
+  if user.try(:authenticate, params[:user][:password])
+    session[:user_id] = user.id
+    redirect '/categories'
+  else
+    redirect '/login?error=true'
+  end
 end
 
 get '/signup' do
-  
+  @error = "If at first you don't succeed, try, try again." if params[:error]
+  erb :'auth/signup'
+end
+
+post '/signup' do
+  user = User.create(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect '/categories'
+  else
+    redirect "/signup?error=true"
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
