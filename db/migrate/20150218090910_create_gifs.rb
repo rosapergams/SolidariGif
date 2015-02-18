cf8a4483f2f8ed937fe87249c9ed95b92b01b409
rosapergams <rosa@pergams.com>
2015-02-18 09:24:23 -0500

    Add migrations

--- db/migrate/20150218090910_create_gifs.rb
+++ db/migrate/20150218090910_create_gifs.rb
@@ -1,4 +1,10 @@
 class CreateGifs < ActiveRecord::Migration
   def change
+    create_table :gifs do |t|
+      t.string :url
+      t.string :caption
+      t.integer :category_id
+      t.integer :user_id
+    end
   end
 end
--- db/migrate/20150218090916_create_comments.rb
+++ db/migrate/20150218090916_create_comments.rb
@@ -1,4 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
+    create_table :comments do |t|
+      t.string :content
+      t.integer :gif_id
+      t.integer :user_id
+    end
   end
 end
