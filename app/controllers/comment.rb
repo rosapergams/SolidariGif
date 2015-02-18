08715edfee5b91b8f414aa90db30975d279d5f34
rosapergams <rosa@pergams.com>
2015-02-18 11:08:45 -0500

    Add create route to comment controller

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -4,3 +4,18 @@ get '/gifs/:id/comments' do |id|
   @gifs = Gif.where(category_id: id)
   erb :'coment/index'
 end
+
+get 'gifs/:id/comments/new' do |id|
+  @user_id = current_user.id
+  @gif_id = id
+  erb :'comment/new'
+end
+
+post '/comments' do
+  @comment = Comment.new(params[:comment])
+  if @comment.save
+    redirect ("/gifs/#{@comment.gif_id}")
+  else
+    redirect ("/gifs/#{@gif.id}/comments/new")
+  end
+end
