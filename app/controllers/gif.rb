7c2f206cd88be3a3d9ecc416c324847f433d1282
rosapergams <rosa@pergams.com>
2015-02-18 10:41:08 -0500

    Add create routes to gif controller

--- app/controllers/gif.rb
+++ app/controllers/gif.rb
@@ -1,3 +1,18 @@
 get '/gifs' do
   redirect 'categories'
 end
+
+get 'categories/:id/gifs/new' do |id|
+  @user_id = current_user.id
+  @category_id = id
+  erb :'gif/new'
+end
+
+post '/gifs' do
+  @gif = Gif.new(params[:gif])
+  if @gif.save
+    redirect ("/gifs/#{{@gif.id}}")
+  else
+    redirect ("/categories")
+  end
+end
