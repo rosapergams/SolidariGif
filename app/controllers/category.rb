8792f1b5ecf9ea403dafe71ada1658b54406813f
rosapergams <rosa@pergams.com>
2015-02-18 10:31:21 -0500

    Add read routes to category controller

--- app/controllers/category.rb
+++ app/controllers/category.rb
@@ -2,3 +2,9 @@ get '/categories' do
   @categories = Category.all
   erb :'category/index'
 end
+
+get 'categories/:id' do |id|
+  @gifs = Gif.where(category_id: id)
+  @category = Category.find(id)
+  erb :'category/show'
+end
